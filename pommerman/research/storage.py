ca19b2ee5337c79d6298ddaff14e15e6166c2c0e
cinjon <cinjon.resnick@gmail.com>
2018-03-28 13:12:48 -0400

    storage: fix the 80 line issues.

--- pommerman/research/storage.py
+++ pommerman/research/storage.py
@@ -3,21 +3,31 @@ from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
 
 
 class RolloutStorage(object):
-    def __init__(self, num_steps, num_processes, obs_shape, action_space, state_size, num_training_per_episode):
-        self.observations = torch.zeros(num_steps + 1, num_training_per_episode, num_processes, *obs_shape)
-        self.states = torch.zeros(num_steps + 1, num_training_per_episode, num_processes, state_size)
-        self.rewards = torch.zeros(num_steps, num_training_per_episode, num_processes, 1)
-        self.value_preds = torch.zeros(num_steps + 1, num_training_per_episode, num_processes, 1)
-        self.returns = torch.zeros(num_steps + 1, num_training_per_episode, num_processes, 1)
-        self.action_log_probs = torch.zeros(num_steps, num_training_per_episode, num_processes, 1)
+    def __init__(self, num_steps, num_processes, obs_shape, action_space,
+                 state_size, num_training_per_episode):
+        self.observations = torch.zeros(
+            num_steps+1, num_training_per_episode, num_processes, *obs_shape)
+        self.states = torch.zeros(
+            num_steps+1, num_training_per_episode, num_processes, state_size)
+        self.rewards = torch.zeros(
+            num_steps, num_training_per_episode, num_processes, 1)
+        self.value_preds = torch.zeros(
+            num_steps+1, num_training_per_episode, num_processes, 1)
+        self.returns = torch.zeros(
+            num_steps+1, num_training_per_episode, num_processes, 1)
+        self.action_log_probs = torch.zeros(
+            num_steps, num_training_per_episode, num_processes, 1)
+                                            
         if action_space.__class__.__name__ == 'Discrete':
             action_shape = 1
         else:
             action_shape = action_space.shape[0]
-        self.actions = torch.zeros(num_steps, num_training_per_episode, num_processes, action_shape)
+        self.actions = torch.zeros(num_steps, num_training_per_episode,
+                                   num_processes, action_shape)
         if action_space.__class__.__name__ == 'Discrete':
             self.actions = self.actions.long()
-        self.masks = torch.ones(num_steps + 1, num_training_per_episode, num_processes, 1)
+        self.masks = torch.ones(num_steps+1, num_training_per_episode,
+                                num_processes, 1)
 
     def cuda(self):
         self.observations = self.observations.cuda()
@@ -29,14 +39,15 @@ class RolloutStorage(object):
         self.actions = self.actions.cuda()
         self.masks = self.masks.cuda()
 
-    def insert(self, step, current_obs, state, action, action_log_prob, value_pred, reward, mask):
-        self.observations[step + 1].copy_(current_obs)
-        self.states[step + 1].copy_(state)
+    def insert(self, step, current_obs, state, action, action_log_prob,
+               value_pred, reward, mask):
+        self.observations[step+1].copy_(current_obs)
+        self.states[step+1].copy_(state)
         self.actions[step].copy_(action)
         self.action_log_probs[step].copy_(action_log_prob)
         self.value_preds[step].copy_(value_pred)
         self.rewards[step].copy_(reward)
-        self.masks[step + 1].copy_(mask)
+        self.masks[step+1].copy_(mask)
 
     def after_update(self):
         self.observations[0].copy_(self.observations[-1])
@@ -46,16 +57,25 @@ class RolloutStorage(object):
     def compute_returns(self, next_value, use_gae, gamma, tau, num_agent=0):
         if use_gae:
             self.value_preds[-1, num_agent] = next_value
+            masks = self.masks
+            value_preds = self.value_preds
             gae = 0
             for step in reversed(range(self.rewards.size(0))):
-                delta = self.rewards[step, num_agent] + gamma * self.value_preds[step + 1, num_agent] * self.masks[step + 1, num_agent] - self.value_preds[step, num_agent]
-                gae = delta + gamma * tau * self.masks[step + 1, num_agent] * gae
-                self.returns[step, num_agent] = gae + self.value_preds[step, num_agent]
+                mask = value_preds[step+1, num_agent]
+                mask *= masks[step+1, num_agent]
+                delta = self.rewards[step, num_agent]
+                delta += gamma * mask - value_preds[step, num_agent]
+                gae = delta + gamma * tau * gae * masks[step+1, num_agent]
+                self.returns[step, num_agent] = value_preds[step, num_agent]
+                self.returns[step, num_agent] += gae
         else:
             self.returns[-1, num_agent] = next_value
             for step in reversed(range(self.rewards.size(0))):
-                self.returns[step, num_agent] = self.returns[step + 1, num_agent] * \
-                                                gamma * self.masks[step + 1, num_agent] + self.rewards[step, num_agent]
+                rewards = self.rewards[step, num_agent]
+                masks = self.masks[step+1, num_agent]
+                next_returns = self.returns[step+1, num_agent]
+                self.returns[step, num_agent] = next_returns * gamma * masks
+                self.returns[step, num_agent] += rewards
 
     def compute_advantages(self):
         return self.returns[:-1] - self.value_preds[:-1]
@@ -72,17 +92,20 @@ class RolloutStorage(object):
 
         batch_size = num_processes * num_steps
         mini_batch_size = batch_size // args.num_mini_batch
-        sampler = BatchSampler(SubsetRandomSampler(range(batch_size)), mini_batch_size, drop_last=False)
+        sampler = BatchSampler(SubsetRandomSampler(range(batch_size)),
+                               mini_batch_size, drop_last=False)
 
-        # We reshape these so that the trajectories per agent instead look like new processes.
-        observations = self.observations.view([num_steps + 1, num_total, *obs_shape])
-        states = self.states.view([num_steps + 1, num_total, state_size])
+        # Reshape so that trajectories per agent  look like new processes.
+        observations = self.observations.view([
+            num_steps+1, num_total, *obs_shape])
+        states = self.states.view([num_steps+1, num_total, state_size])
         rewards = self.rewards.view([num_steps, num_total, 1])
-        value_preds = self.value_preds.view([num_steps + 1, num_total, 1])
-        returns = self.returns.view([num_steps + 1, num_total, 1])
+        value_preds = self.value_preds.view([num_steps+1, num_total, 1])
+        returns = self.returns.view([num_steps+1, num_total, 1])
         actions = self.actions.view([num_steps, num_total, *action_shape])
-        action_log_probs = self.action_log_probs.view([num_steps, num_total, 1])
-        masks = self.masks.view([num_steps + 1, num_total, 1])
+        action_log_probs = self.action_log_probs.view([
+            num_steps, num_total, 1])
+        masks = self.masks.view([num_steps+1, num_total, 1])
 
         for indices in sampler:
             indices = torch.LongTensor(indices)
@@ -90,13 +113,28 @@ class RolloutStorage(object):
             if advantages.is_cuda:
                 indices = indices.cuda()
 
-            observations_batch = observations[:-1].contiguous().view((args.num_steps*num_total), *observations.size()[2:])[indices]
-            states_batch = states[:-1].contiguous().view((args.num_steps*num_total), 1)[indices]
-            actions_batch = actions.contiguous().view((args.num_steps*num_total), 1)[indices]
-            return_batch = returns[:-1].contiguous().view((args.num_steps*num_total), 1)[indices]
-            masks_batch = masks[:-1].contiguous().view((args.num_steps*num_total), 1)[indices]
-            old_action_log_probs_batch = action_log_probs.contiguous().view((args.num_steps*num_total), 1)[indices]
+            observations_batch = observations[:-1] \
+                                 .contiguous() \
+                                 .view((args.num_steps*num_total),
+                                       *observations.size()[2:])[indices]
+            states_batch = states[:-1] \
+                           .contiguous() \
+                           .view((args.num_steps*num_total), 1)[indices]
+            actions_batch = actions \
+                            .contiguous() \
+                            .view((args.num_steps*num_total), 1)[indices]
+            return_batch = returns[:-1] \
+                           .contiguous() \
+                           .view((args.num_steps*num_total), 1)[indices]
+            masks_batch = masks[:-1] \
+                          .contiguous() \
+                          .view((args.num_steps*num_total), 1)[indices]
+            old_action_log_probs_batch = \
+                                action_log_probs \
+                                .contiguous() \
+                                .view((args.num_steps*num_total), 1)[indices]
             adv_targ = advantages.contiguous().view(-1, 1)[indices]
 
             yield observations_batch, states_batch, actions_batch, \
-                return_batch, masks_batch, old_action_log_probs_batch, adv_targ
+                return_batch, masks_batch, old_action_log_probs_batch, \
+                adv_targ
