591a02a9fa5c63ac8b8bafccbac5e394ffa99148
Cinjon Resnick <cinjon@fb.com>
2018-05-18 12:02:05 -0400

    utils: dont record kl loss if it's NAN.

--- pommerman/research/utils.py
+++ pommerman/research/utils.py
@@ -295,7 +295,7 @@ def log_to_tensorboard(writer, num_epoch, num_episodes, total_steps,
         writer.add_scalar('entropy_step', mean_dist_entropy, total_steps)
         writer.add_scalar('action_loss_step', mean_action_loss, total_steps)
         writer.add_scalar('value_loss_step', mean_value_loss, total_steps)
-    if mean_kl_loss:
+    if mean_kl_loss and not np.isnan(mean_kl_loss):
         writer.add_scalar('kl_loss_step', mean_kl_loss, total_steps)
     writer.add_scalar('total_loss_step', mean_total_loss, total_steps)
 
@@ -345,7 +345,7 @@ def log_to_tensorboard(writer, num_epoch, num_episodes, total_steps,
         writer.add_scalar('entropy_epi', mean_dist_entropy, num_episodes)
         writer.add_scalar('action_loss_epi', mean_action_loss, num_episodes)
         writer.add_scalar('value_loss_epi', mean_value_loss, num_episodes)
-    if mean_kl_loss:
+    if mean_kl_loss and not np.isnan(mean_kl_loss):
         writer.add_scalar('kl_loss_epi', mean_kl_loss, num_episodes)
     writer.add_scalar('total_loss_epi', mean_total_loss, num_episodes)
 
@@ -402,7 +402,7 @@ def log_to_tensorboard(writer, num_epoch, num_episodes, total_steps,
         writer.add_scalar('entropy_epoch', mean_dist_entropy, num_epoch)
         writer.add_scalar('action_loss_epoch', mean_action_loss, num_epoch)
         writer.add_scalar('value_loss_epoch', mean_value_loss, num_epoch)
-    if mean_kl_loss:
+    if mean_kl_loss and not np.isnan(mean_kl_loss):
         writer.add_scalar('kl_loss_epoch', mean_kl_loss, num_epoch)
     if distill_factor > 0:
         writer.add_scalar('kl_factor_epoch', distill_factor, num_epoch)
