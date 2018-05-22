64f6f711dc0963ec6fde0eaf33c67a91bf353140
Cinjon Resnick <cinjon@fb.com>
2018-05-22 12:00:21 -0400

    configs: team 8x8.

--- pommerman/configs.py
+++ pommerman/configs.py
@@ -272,6 +272,27 @@ def ffa_v0_8x8_env():
     return locals()
 
 
+def team_v0_8x8_env():
+    """Start up a team config with dense reward and a lower complexity board."""
+    env = envs.v0.Pomme
+    game_type = constants.GameType.Team
+    env_entry_point = 'pommerman.envs.v0:Pomme'
+    env_id = 'PommeFFA8x8-v0'
+    env_kwargs = {
+        'game_type': game_type,
+        'board_size': constants.BOARD_SIZE_8,
+        'agent_view_size': constants.AGENT_VIEW_SIZE,
+        'num_rigid': constants.NUM_RIGID_8,
+        'num_wood': constants.NUM_WOOD_8,
+        'num_items': constants.NUM_ITEMS_8,
+        'max_steps': constants.MAX_STEPS_8,
+        'render_fps': constants.RENDER_FPS,
+    }
+    agent = characters.Bomber(bomb_life=constants.DEFAULT_BOMB_LIFE_8,
+                              blast_strength=constants.DEFAULT_BLAST_STRENGTH_8)
+    return locals()
+
+
 def team_v0_env():
     """Start up a team config with the default settings."""
     env = envs.v0.Pomme
