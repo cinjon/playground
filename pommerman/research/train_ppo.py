361a07275068ff65f99cee8965db71b5b95babf0
Cinjon Resnick <cinjon@fb.com>
2018-05-22 09:47:22 -0600

    uniformbounds numbers.

--- pommerman/research/train_ppo.py
+++ pommerman/research/train_ppo.py
@@ -118,15 +118,16 @@ def train():
         uniform_v_prior = 0
         envs.set_uniform_v(uniform_v)        
     elif args.state_directory_distribution == 'uniformBoundsA':
-        uniform_v = 33
+        # (0, 32), (24, 64), (56, 128), (120, 256), (248, 512), ...
+        uniform_v = 32
         uniform_v_factor = 2
-        uniform_v_incr = 3500
+        uniform_v_incr = 2500
         uniform_v_prior = 0
         envs.set_uniform_v(uniform_v)
     elif args.state_directory_distribution == 'uniformBoundsB':
-        uniform_v = 33
+        uniform_v = 32
         uniform_v_factor = 2
-        uniform_v_incr = 4500
+        uniform_v_incr = 4000
         uniform_v_prior = 0
         envs.set_uniform_v(uniform_v)
 
