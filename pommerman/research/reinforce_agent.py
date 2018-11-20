wip: 1. fixed advantage computation when using reinforce (use only returns no values); 2. store entire log probs for all possible actions (instead of for a single action the one recommended); 3. need to select the log prob for the action taken by the behavioral policy to compute the importance weight

pommerman/research/reinforce_agent.py
@@ -100,7 +100,7 @@ class ReinforceAgent(ResearchAgent):
         for num_agent, next_value in enumerate(next_value_agents):
             self._rollout.compute_returns(next_value, use_gae, gamma, tau,
                                           num_agent)
-        advantages = self._rollout.compute_advantages()
+        advantages = self._rollout.compute_advantages(reinforce=True)
         diff = (advantages - advantages.mean())
         advantages = diff / (advantages.std() + 1e-5)
         return advantages
@@ -137,8 +137,7 @@ class ReinforceAgent(ResearchAgent):
         self._rollout.insert(step, current_obs, states, action,
                              action_log_prob, value, reward, mask,
                              action_log_prob_distr, dagger_prob_distr,
-                             expert_action_log_prob=None,
-                             training_action_log_prob=None)
+                             expert_action_log_prob, training_action_log_prob)
 
 
     def reinforce(self, advantages, num_mini_batch, batch_size, num_steps,
@@ -165,11 +164,11 @@ class ReinforceAgent(ResearchAgent):
                 Variable(actions_batch))
             values, action_log_probs, dist_entropy, states = result
 
-            import pdb; pdb.set_trace()
             behavior_action_probs_batch = kl_factor * torch.exp(Variable(expert_action_log_probs_batch)) + \
                 (1 - kl_factor) * torch.exp(Variable(training_action_log_probs_batch))
 
-            # TODO: we don't need to multiply the entire advantage by that ratio but only the reward
+            # TODO: take only the probs corresponding to the taken action for training and behavior
+
             adv_targ = Variable(adv_targ)
             if use_is:
                 adv_targ *= torch.exp(Variable(training_action_log_probs_batch)) / behavior_action_probs_batch

pommerman/research/storage.py
@@ -31,10 +31,11 @@ class RolloutStorage(object):
             num_steps, num_training_per_episode, num_processes, action_space.n)
         self.action_log_probs_distr = torch.zeros(
             num_steps, num_training_per_episode, num_processes, action_space.n)
+
         self.expert_action_log_probs = torch.zeros(
-            num_steps, num_training_per_episode, num_processes, 1)
+            num_steps, num_training_per_episode, num_processes, action_space.n)
         self.training_action_log_probs = torch.zeros(
-            num_steps, num_training_per_episode, num_processes, 1)
+            num_steps, num_training_per_episode, num_processes, action_space.n)
 
     def cuda(self):
         self.observations = self.observations.cuda()
@@ -99,9 +100,11 @@ class RolloutStorage(object):
                 self.returns[step, num_agent] = next_returns * gamma * masks
                 self.returns[step, num_agent] += rewards
 
-    def compute_advantages(self):
-        import pdb; pdb.set_trace()
-        return self.returns[:-1] - self.value_preds[:-1]
+    def compute_advantages(self, reinforce=False):
+        if reinforce:
+            return self.returns[:-1]
+        else:
+            return self.returns[:-1] - self.value_preds[:-1]
 
     def feed_forward_generator(self, advantages, num_mini_batch, batch_size,
                                num_steps, action_space, kl_factor, use_is):
@@ -142,10 +145,10 @@ class RolloutStorage(object):
                 [num_steps, num_total, *distr_shape])
 
         if use_is:
-            expert_action_log_probs = self.expert_action_log_probs.view([
-                num_steps, num_total, 1])
-            training_action_log_probs = self.training_action_log_probs.view([
-                num_steps, num_total, 1])
+            expert_action_log_probs = self.expert_action_log_probs.view(
+                [num_steps, num_total, *distr_shape])
+            training_action_log_probs = self.training_action_log_probs.view(
+                [num_steps, num_total, *distr_shape])
 
         counter = 0
         for indices in sampler:
@@ -197,10 +200,10 @@ class RolloutStorage(object):
             if use_is:
                 expert_action_log_probs_batch = expert_action_log_probs \
                                                     .contiguous() \
-                                                    .view((num_steps*num_total), 1)[indices]
+                                                    .view((num_steps*num_total), action_space.n)[indices]
                 training_action_log_probs_batch = training_action_log_probs \
                                                     .contiguous() \
-                                                    .view((num_steps*num_total), 1)[indices]
+                                                    .view((num_steps*num_total), action_space.n)[indices]
             else:
                 expert_action_log_probs_batch = None
                 training_action_log_probs_batch = None
@@ -245,9 +248,9 @@ class RolloutStorage(object):
 
         if use_is:
             expert_action_log_probs = self.expert_action_log_probs.view(
-                                        [num_steps, num_total, 1])
+                                        [num_steps, num_total, *distr_shape])
             training_action_log_probs = self.training_action_log_probs.view(
-                                        [num_steps, num_total, 1])
+                                        [num_steps, num_total, *distr_shape])
 
         for start_ind in range(0, num_processes, num_envs_per_batch):
             observations_batch = []
@@ -287,18 +290,18 @@ class RolloutStorage(object):
                     # TODO: Change the hard-coded 6.
                     dagger_probs_distr_batch.append(dagger_probs_distr \
                                                .contiguous() \
-                                               .view((num_steps*num_total), 6)[indices])
+                                               .view((num_steps*num_total), action_space.n)[indices])
                     action_log_probs_distr_batch.append(action_log_probs_distr \
                                                    .contiguous() \
-                                                   .view((num_steps*num_total), 6)[indices])
+                                                   .view((num_steps*num_total), action_space.n)[indices])
 
                 if use_is:
                     expert_action_log_probs_batch.append(expert_action_log_probs \
                                                         .contiguous() \
-                                                        .view((num_steps*num_total), 1)[indices])
+                                                        .view((num_steps*num_total), action_space.n)[indices])
                     training_action_log_probs_batch.append(training_action_log_probs \
                                                         .contiguous() \
-                                                        .view((num_steps*num_total), 1)[indices])
+                                                        .view((num_steps*num_total), action_space.n)[indices])
 
             observations_batch = torch.cat(observations_batch, 0) \
                                  .view(num_envs_per_batch, *observations.size()[2:])
@@ -317,18 +320,18 @@ class RolloutStorage(object):
 
             if kl_factor > 0:
                 dagger_probs_distr_batch = torch.cat(dagger_probs_distr_batch, 0) \
-                                           .view(num_envs_per_batch, 6)
+                                           .view(num_envs_per_batch, action_space.n)
                 action_log_probs_distr_batch = torch.cat(action_log_probs_distr_batch, 0) \
-                                               .view(num_envs_per_batch, 6)
+                                               .view(num_envs_per_batch, action_space.n)
             else:
                 dagger_probs_distr_batch = None
                 action_log_probs_distr_batch = None
 
             if use_is:
                 expert_action_log_probs_batch = torch.cat(expert_action_log_probs_batch, 0) \
-                                             .view(num_envs_per_batch, 1)
+                                             .view(num_envs_per_batch, action_space.n)
                 training_action_log_probs_batch = torch.cat(training_action_log_probs_batch, 0) \
-                                             .view(num_envs_per_batch, 1)
+                                             .view(num_envs_per_batch, action_space.n)
             else:
                 expert_action_log_probs_batch = None
                 training_action_log_probs_batch = None

pommerman/research/train_ppo.py
@@ -421,6 +421,8 @@ def train():
         if distill_expert == 'DaggerAgent':
             distill_agent = utils.load_distill_agent(obs_shape, action_space,
                                 board_size, args)
+            # TODO: what does this do again? looks like it does not force
+            # the agent to acat deterministically
             distill_agent.set_eval()
             # NOTE: We have to call init_agent, but the agent_id won't matter
             # because we will use the observations from the ppo_agent.
@@ -504,6 +506,8 @@ def train():
         final_dist_entropies = [[] for agent in range(len(training_agents))]
     if do_distill and not use_importance_sampling:
         final_kl_losses = [[] for agent in range(len(training_agents))]
+    else:
+        final_kl_losses = []
     final_total_losses =  [[] for agent in range(len(training_agents))]
 
     def update_current_obs(obs):
@@ -720,6 +724,7 @@ def train():
             utils.save_agents("ppo-", num_epoch, training_agents, total_steps,
                               num_episodes, args, suffix, clear_saved=True)
 
+        # NOTE: why do we need this??
         for agent in training_agents:
             agent.set_eval()
 
@@ -972,20 +977,15 @@ def train():
                             *data, deterministic=True)
                         dagger_prob_distr.append(probs)
 
-                import pdb; pdb.set_trace()
                 if use_importance_sampling:
-
                     data = training_agent.get_rollout_data(step, 0)
                     bc_result = distill_agent.act_on_data(
                         *data, deterministic=False)
-                    bc_action_prob = bc_result[2]
+                    bc_action_prob = bc_result[5]
 
                     reinf_result = training_agent.actor_critic_act(
                         step, 0, deterministic=False)
-                    reinf_action_prob = reinf_result[2]
-
-                    # expert_action_log_prob = torch.log(distill_factor * torch.exp(bc_action_prob) + \
-                    #     (1 - distill_factor) * torch.exp(reinf_result))
+                    reinf_action_prob = reinf_result[5]
 
                     action_log_prob_expert.append(bc_action_prob)
                     action_log_prob_training.append(reinf_action_prob)
@@ -1617,8 +1617,10 @@ def train():
                 final_value_losses =  [[] for agent in range(len(training_agents))]
                 final_dist_entropies = [[] for agent in \
                                         range(len(training_agents))]
-            if do_distill and not_use_importance_sampling:
+            if do_distill and not use_importance_sampling:
                 final_kl_losses = [[] for agent in range(len(training_agents))]
+            else:
+                final_kl_losses = []
             final_total_losses =  [[] for agent in range(len(training_agents))]
 
             count_stats = defaultdict(int)